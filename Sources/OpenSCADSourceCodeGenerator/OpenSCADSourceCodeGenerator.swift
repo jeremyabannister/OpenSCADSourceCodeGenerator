0e21d220c222eb71074b04173a117e309f2b5cbb
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2018-07-05 01:01:15 -0700

    Updated to accommodate breaking API changes in Object3D 0.2.0;

--- Sources/OpenSCADSourceCodeGenerator/OpenSCADSourceCodeGenerator.swift
+++ Sources/OpenSCADSourceCodeGenerator/OpenSCADSourceCodeGenerator.swift
@@ -8,11 +8,11 @@
 @_exported import Object3D
 
 public struct OpenSCADSourceCodeGenerator {
-  public static func sourceCode (for object: Object3DEnum) -> String {
+  public static func sourceCode (for object: RawObject3D) -> String {
     return OpenSCADSourceCodeGenerator.sourceCode(for: object, shouldAddVariables: true)
   }
   
-  internal static func sourceCode (for object: Object3DEnum, shouldAddVariables: Bool) -> String {
+  internal static func sourceCode (for object: RawObject3D, shouldAddVariables: Bool) -> String {
     var generatedSourceCode = ""
     
     switch object {
@@ -23,9 +23,9 @@ public struct OpenSCADSourceCodeGenerator {
       let translation = OpenSCADSourceCodeGenerator.translationString(for: position)
       generatedSourceCode = "\(translation)cylinder(r1 = \(lowerRadius.asOpenSCADVariable), r2 = \(upperRadius.asOpenSCADVariable), h = \(depth.asOpenSCADVariable), $fn=\(100.0.asOpenSCADVariable));"
       
-    case .union(position: let position, objectEnums: let objectEnums):
+    case .union(position: let position, rawObjects: let rawObjects):
       let translation = OpenSCADSourceCodeGenerator.translationString(for: position)
-      let interiorSourceCode = objectEnums.map({ OpenSCADSourceCodeGenerator.sourceCode(for: $0, shouldAddVariables: false) }).joined(separator: "\n")
+      let interiorSourceCode = rawObjects.map({ OpenSCADSourceCodeGenerator.sourceCode(for: $0, shouldAddVariables: false) }).joined(separator: "\n")
       generatedSourceCode = "\(translation)union () {\n\(interiorSourceCode)\n}"
       
     case .difference(position: let position, original: let original, subtractions: let subtractions):
