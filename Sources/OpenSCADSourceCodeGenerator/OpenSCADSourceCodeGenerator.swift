c0e85f6ec5cb48a842da404e6ea45141aec9eca7
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2018-07-06 01:10:19 -0700

    Made resolution a configurable `static var` with two enum cases, `ResolutionMode.development` and `ResolutionMode.production`;

--- Sources/OpenSCADSourceCodeGenerator/OpenSCADSourceCodeGenerator.swift
+++ Sources/OpenSCADSourceCodeGenerator/OpenSCADSourceCodeGenerator.swift
@@ -8,6 +8,8 @@
 @_exported import Object3D
 
 public struct OpenSCADSourceCodeGenerator {
+  public static var resolutionMode: ResolutionMode = .production
+  
   public static func sourceCode (for object: RawObject3D) -> String {
     return OpenSCADSourceCodeGenerator.sourceCode(for: object, shouldAddVariables: true)
   }
@@ -21,7 +23,7 @@ public struct OpenSCADSourceCodeGenerator {
       generatedSourceCode = "\(translation)cube(\(size.formattedForOpenSCAD));"
     case .cylinder(position: let position, lowerRadius: let lowerRadius, upperRadius: let upperRadius, depth: let depth):
       let translation = OpenSCADSourceCodeGenerator.translationString(for: position)
-      generatedSourceCode = "\(translation)cylinder(r1 = \(lowerRadius.asOpenSCADVariable), r2 = \(upperRadius.asOpenSCADVariable), h = \(depth.asOpenSCADVariable), $fn=\(100.0.asOpenSCADVariable));"
+      generatedSourceCode = "\(translation)cylinder(r1 = \(lowerRadius.asOpenSCADVariable), r2 = \(upperRadius.asOpenSCADVariable), h = \(depth.asOpenSCADVariable)\(resolutionMode.resolutionModificationString));"
       
     case .union(position: let position, rawObjects: let rawObjects):
       let translation = OpenSCADSourceCodeGenerator.translationString(for: position)
@@ -46,6 +48,20 @@ public struct OpenSCADSourceCodeGenerator {
   
   internal static func translationString (for position: Position3D) -> String {
     return position == .zero ? "" : "translate(\(position.formattedForOpenSCAD)) "
+  }
+}
+
+
+private var resolutionMode: OpenSCADSourceCodeGenerator.ResolutionMode { return OpenSCADSourceCodeGenerator.resolutionMode }
+extension OpenSCADSourceCodeGenerator {
+  public enum ResolutionMode {
+    case development, production
+    internal var resolutionModificationString: String {
+      switch self {
+      case .development: return ""
+      case .production: return ", $fn=\(100.0.asOpenSCADVariable)"
+      }
+    }
   }
 }
 
